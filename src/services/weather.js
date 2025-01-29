4670f457f227434c1b88b836edf64a637f0038a1
Kristofer <154324174+KristoferAasa@users.noreply.github.com>
2025-01-29 13:32:01 +0200

    Added weather api

--- src/services/weather.js
+++ src/services/weather.js
@@ -4,8 +4,11 @@ const api_key = import.meta.env.VITE_WEATHER_API_KEY;
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 const getWeather = (capital) => {
-  const url = `${baseUrl}?q=${capital}&appid=${api_key}&units=metric`;
-  return axios.get(url).then((response) => response.data);
+  const url = `${baseUrl}?q=${capital}&appid=${api_key}`;
+  return axios.get(url).then((response) => response.data).catch((err) => {
+    console.error("Error:", err.response?.data || err.message);
+    throw err;
+  });
 };
 
 export default getWeather;
